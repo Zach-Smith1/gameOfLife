8c2a5a7fecbdacb2a6e2341bd5fcf95e2d077237
Zach Smith <zachsmithglobal@icloud.com>
2024-08-06 13:12:44 -0700

    renamed words/buttons

--- src/Life.js
+++ src/Life.js
@@ -206,11 +206,11 @@ const Life = () => {
           {/* <button onClick={pause}>{paused === true ? '\u{25B6}' : <strong>{'\u{23F8}'}</strong> }</button>*/}
         <button onClick={pause}>{'\u{25B6}'} <strong id='pause'>{'\u{23F8}'}<strong/></strong></button>
           {/* <button onClick={() => setTick(lastTick)}>GO</button> */}
-          <button id='arrowButton' onClick={nextGen}>1 Step</button>
+          <button id='arrowButton' onClick={nextGen}>+1 Gen</button>
           <button id='arrowButton' onClick={speedUp}>&#9650;</button>
           <button id='arrowButton' onClick={slowDown}>&#9660;</button>
           <button id='clearButton' onClick={newMatrix}>Clear</button>
-          <span className='info'>{paused ? 'Paused' : `${Math.round((1000/tick)*100)/100} generations/ second`}</span>
+          <span className='info'>{paused ? 'Paused' : `${Math.round((1000/tick)*100)/100} gen/ sec`}</span>
         </span>
       </div>
     </div>
